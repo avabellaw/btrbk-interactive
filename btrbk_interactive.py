9d8a6ba72fefd3b3e1b12e495b95eac95fa0eeb9
avabellaw <avabellawilliams@gmail.com>
2025-11-08 19:52:09 +0000

    List snapshots and group by subvol and date

--- btrbk_interactive.py
+++ btrbk_interactive.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import calendar
+from itertools import groupby
 
 snapshots_dir = sys.argv[1]
 SNAPSHOT_REGEX = "^@([a-zA-Z-]*).([0-9]{4})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})$"
@@ -14,11 +15,13 @@ class Snapshot():
         self.subvol, self.year, self.month, self.day, self.hr, self.min = \
             self.data
 
+        self.month_name = calendar.month_name[int(self.month)]
+
         self.snapshot_name = snapshot_name
         self.path = f"{snapshots_dir}/{snapshot_name}"
 
     def __str__(self):
-        return f"@{self.subvol} {self.day} {calendar.month_name[int(self.month)][:3]}"
+        return f"@{self.subvol}: {self.hr}:{self.min} {self.day} {self.month_name[:3]} {self.year}"
 
 
 def init():
@@ -42,12 +45,17 @@ def get_snapshot_arr():
 class ManageSnapshots():
     def __init__(self, snapshots):
         self.snapshots = snapshots
-        self.parent_dir = ""
-        self.subvol = ""
+        self.snapshots_by_subvol = groupby(snapshots, key=lambda s: s.subvol)
 
     def list(self):
-        for s in self.snapshots:
-            print(s)
+        for subvol, snapshots in self.snapshots_by_subvol:
+            print(f"----\nSubvolume: @{subvol}\n----")
+
+            for date, snapshots_by_date in groupby(snapshots, key=lambda s: f"{s.day} {s.month_name[:3]} {s.year}"):
+                print(f"\n{date}")
+
+                for s in snapshots_by_date:
+                    print(f"- {s.hr}:{s.min}")
 
 
 init()
